0fe5d8173f77519869d3d3cc7c507e1832de81e6
huynhtrongdai007 <50190444+huynhtrongdai007@users.noreply.github.com>
2021-03-15 21:19:56 +0700

    upcode
    create pages admin

---
+++ resources/views/admin/master_layout.blade.php
@@ -0,0 +1,11 @@
+@include('admin.blocks.header')
+@include('admin.blocks.sidebar')
+        <div class="grid_10">
+            <div class="box round first grid">
+                <h2> Dashbord</h2>
+                <div class="block">               
+                  Welcome admin panel        
+                </div>
+            </div>
+        </div>
+@include('admin.blocks.footer')
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('master_layout');
-});
+// Route::get('/', function () {
+//     return view('master_layout');
+// });
 
+Route::get('/', function () {
+    return view('admin.master_layout');
+});
 
